a09c27e22318a411778f9e54d35adccd25801ed1
vileven <vileven11@icloud.com>
2017-05-12 23:11:40 +0300

    Added heroku and local database profiles

---
+++ src/main/java/api/Application.java
@@ -0,0 +1,21 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/test/java/api/ApplicationTest.java
@@ -0,0 +1,10 @@
+package api;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Vileven on 12.05.17.
+ */
+public class ApplicationTest {
+
+}
