e24bef6ca14f1b21735fd34010cb4df2d19c5a40
vileven <vileven11@icloud.com>
2017-05-13 20:06:31 +0300

    Added course model

--- src/main/java/api/models/Course.java
+++ src/main/java/api/models/Course.java
@@ -3,5 +3,34 @@ package api.models;
 /**
  * Created by Vileven on 13.05.17.
  */
-public class Course {
+public class Course extends Model<Long> {
+
+    private Long id;
+    private String name;
+
+    public Course(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
 }
