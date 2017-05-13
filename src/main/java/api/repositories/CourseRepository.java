b71151e40c33c42a09a89056de624998557557d7
vileven <vileven11@icloud.com>
2017-05-13 20:15:10 +0300

    added javafx dependency

--- src/main/java/api/repositories/CourseRepository.java
+++ src/main/java/api/repositories/CourseRepository.java
@@ -1,7 +1,21 @@
 package api.repositories;
 
+import api.models.Course;
+import org.jetbrains.annotations.Nullable;
+
 /**
  * Created by Vileven on 13.05.17.
  */
 public interface CourseRepository {
+
+    @Nullable
+    Course create(Course course);
+
+    void updateName(long id, String name);
+
+    @Nullable
+    Course find(long id);
+
+    Course findByName(String name);
+
 }
